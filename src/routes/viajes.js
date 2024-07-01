e58db721de81e2a736501a2881ae31ec166f6014
SantinoBartolucci <santi.barto12@gmail.com>
2024-07-01 17:11:16 -0300

    wip

--- src/routes/viajes.js
+++ src/routes/viajes.js
@@ -9,4 +9,17 @@ router.get('/viajar', isLoggedIn, (req, res) => {
 	res.render('pages/viajes/viajar', { user });
 });
 
+router.post('/viajar', isLoggedIn, (req, res) => {
+	const user = req.user[0];
+	const { box, place_from, place_to, date_start, date_end } = req.body;
+
+	console.log(box);
+	console.log(place_from);
+	console.log(place_to);
+	console.log(date_start);
+	console.log(date_end);
+
+	res.render('pages/viajes/viajar', { user });
+});
+
 module.exports = router;
