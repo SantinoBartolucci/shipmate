37134cc560ae60fa242d7ee7d153c288017c0faa
/Santino <74921908+SantinoBartolucci@users.noreply.github.com>
2024-07-08 14:22:46 -0300

    Backend viajar (#10)
    * wip
    
    * .

--- src/routes/chats.js
+++ src/routes/chats.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/chats/:id', (req, res) => {
+const { isLoggedIn } = require('../helpers/isLogged');
+
+router.get('/chats/:id', isLoggedIn, (req, res) => {
 	const user = req.user[0];
 	res.render('pages/chats/index', { layout: 'chats', user }); // { user });
 });
--- src/routes/viajes.js
+++ src/routes/viajes.js
@@ -2,10 +2,26 @@ const { Router } = require('express');
 const router = Router();
 
 const { isLoggedIn } = require('../helpers/isLogged');
+const pool = require('../database');
 
 router.get('/viajar', isLoggedIn, (req, res) => {
 	const user = req.user[0];
 
+	res.render('pages/viajes/viajar', { user });
+});
+
+router.post('/viajar', isLoggedIn, async (req, res) => {
+	const user = req.user[0];
+	const { place_from, place_to, date_start, date_end } = req.body;
+
+	await pool.query('INSERT INTO viajes (place_from, place_to, date_start, date_end) VALUES (?, ?, ?, ?)', [
+		place_from,
+		place_to,
+		date_start,
+		date_end,
+	]);
+
+	req.flash('success_msg', '¡Viaje registrado correctamente!');
 	res.render('pages/viajes/viajar', { user });
 });
 
