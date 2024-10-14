6ea4837943f2d1870a96bfe33c465094642cd53a
SantinoBartolucci <santi.barto12@gmail.com>
2024-10-14 16:41:21 -0300

    Mercado Pago Working

--- src/routes/pedidoTramite.js
+++ src/routes/pedidoTramite.js
@@ -5,6 +5,9 @@ const { isLoggedIn } = require('../helpers/isLogged');
 const pool = require('../database');
 const { IsSqlInjectionAttempt } = require('../helpers/isSQL');
 
+const { MercadoPagoConfig, Preference, Payment } = require("mercadopago");
+const meli = new MercadoPagoConfig({accessToken: "APP_USR-7396164588534070-101413-216e559d39556bf72373d044a102fc31-2035049805"});
+
 router.get('/productoenoferta/:id', isLoggedIn, async (req, res) => {
 	const user = req.user[0];
 	const product_id = parseInt(req.params.id);
@@ -66,6 +69,27 @@ router.get('/deleteOffer/:productid/:id', isLoggedIn, async (req, res) => {
 
 router.get('/acceptOffer/:id', isLoggedIn, async (req, res) => {
 	const offer_id = parseInt(req.params.id);
+	// console.log(offer_id + " 1");
+
+	const [offersInfo] = await pool.query(
+		'SELECT id_pedido, id_usuario, total FROM ofertas where id = ? ',
+		[offer_id]
+	);
+
+	const [product] = await pool.query(
+		'select name, total, amount from pedidos where id = (?) ',
+		[offersInfo.id_pedido]
+	);
+
+	product.total = offersInfo.total;
+
+	let url = await CreateOrder(product, offer_id);
+	// console.log(url);
+	res.redirect(url);
+});
+
+router.get('/payment/success/:offerId', isLoggedIn, async (req, res) => {
+	const offer_id = parseInt(req.params.offerId);
 	// console.log(offer_id);
 
 	const [offersInfo] = await pool.query(
@@ -106,6 +130,14 @@ router.get('/acceptOffer/:id', isLoggedIn, async (req, res) => {
 	res.redirect('/chats/' + productInfo.user_id);
 });
 
+router.get('/payment/failure', isLoggedIn, (req, res) => {
+    res.send('Payment failed. Please try again.');
+});
+
+router.get('/payment/pending', isLoggedIn, (req, res) => {
+    res.send('Payment is pending. Please check back later.');
+});
+
 async function GetProduct(id) {
 	const product = await pool.query(
 		'SELECT id, user_id, name, total, from_place, to_place, details FROM pedidos WHERE id=?',
@@ -140,6 +172,29 @@ async function GetOffers(pedidoId, userId) {
 	}
 
 	return results;
+}
+
+async function CreateOrder(product, offer_id) {
+	const result = await new Preference(meli).create({
+        body: {
+            items: [
+                {
+                    title: product.name,
+                    unit_price: product.total,
+                    currency_id: "ARG",
+                    quantity: product.amount,
+                }
+            ],
+            back_urls: {
+                success: `http://localhost:3000/payment/success/${offer_id}`,
+                failure: `http://localhost:3000/payment/failure`,
+                pending: `http://localhost:3000/payment/pending`
+            },
+            auto_return: 'approved',
+        }
+    });
+    
+	return result.init_point;
 }
 
 module.exports = router;
